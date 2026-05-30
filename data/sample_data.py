ac923d24cae1c6753000610bf996874318728e5d
Dhiraj123-star <gdhiraj499@gmail.com>
2026-05-30 17:22:41 +0530

    feat: add sample sales dataset

---
+++ data/sample_data.py
@@ -0,0 +1,16 @@
+SALES_DATA = """
+Monthly Sales Data (2024)
+
+January:   $12,400
+February:  $9,800
+March:     $15,200
+April:     $14,700
+May:       $18,300
+June:      $16,900
+July:      $11,200
+August:    $13,500
+September: $17,800
+October:   $20,100
+November:  $22,400
+December:  $25,000
+"""
