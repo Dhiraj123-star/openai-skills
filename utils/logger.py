f9680786d37c3a31134726b3c5091f7122b1ccca
Dhiraj123-star <gdhiraj499@gmail.com>
2026-06-02 01:27:02 +0530

    feat: add structured logging

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,29 @@
-def print_banner(title):
-    print("="*70)
-    print(title)
-    print("="*70)
+import logging
+import sys
+
+def setup_logger():
+    logger = logging.getLogger("openai-skills")
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+
+    formatter = logging.Formatter(
+        fmt= (
+            "%(asctime)s | "
+            "%(levelname)s | "
+            "%(name)s | "
+            "%(message)s"
+        )
+    )
+
+    handler.setFormatter(formatter)
+    
+    logger.addHandler(handler)
+
+    return logger
+
+logger = setup_logger()
