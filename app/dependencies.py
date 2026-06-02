0cceae46c5108ae6e08b4d6f25333d5b6573beed
Dhiraj123-star <gdhiraj499@gmail.com>
2026-06-03 01:08:54 +0530

    feat: migrate services to async OpenAI SDK

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,6 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 
 from config.settings import OPENAI_API_KEY
 
-def get_openai_client()-> OpenAI:
-    return OpenAI(api_key=OPENAI_API_KEY)
+def get_openai_client():
+    return AsyncOpenAI(api_key=OPENAI_API_KEY)
